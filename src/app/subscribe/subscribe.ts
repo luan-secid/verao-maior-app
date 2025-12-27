e5e6d16c0f23cf17be5a830ea05027653dcd80c8
Luan Alves <luan@secid.pr.gov.br>
2025-12-27 18:26:58 -0300

    versão 0.4.2 com correção no birthday input

--- src/app/subscribe/subscribe.ts
+++ src/app/subscribe/subscribe.ts
@@ -5,7 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { CepService } from '../core/api/cep.service';
 import { UserService } from '../core/api/user.service';
 import { User } from '../core/api/models/user.model';
@@ -14,6 +14,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { provideNgxMask, NgxMaskDirective } from 'ngx-mask';
 import { parse } from 'date-fns';
+
 
 @Component({
   selector: 'app-subscribe',
